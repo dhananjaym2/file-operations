e0cea0083e9e3c30a1cf62129e5a62f2dc52429c
Dhananjay <dhananjaymohnot@gmail.com>
2018-08-30 00:16:23 +0530

    psuedo steps or algorithm as per my first understanding of the task. Not tested yet.

---
+++ search-specified-pattern-and-copy-matching-line-to-new-file.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+# Program to search-specified-pattern-and-copy-matching-line-to-new-file
+
+# read input user specified file name, check if it exists
+
+# read output file name, check if it exists, then prompt user to replace it.
+
+# read a line into a buffer and search for that specifed pattern in that line
+
+# if matched pattern is found then append that to the copy file
+
+# Prompt user that operation is complete.
+echo "Operation complete."
